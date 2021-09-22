7eec9c9e9b7f81975bbcf6d0d1063889038f2d4c
Moisés Bryan <bryanmoises10@gmail.com>
2021-09-22 17:03:53 -0300

    Add files via upload

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,39 +1,41 @@
 var canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d");
 
-canvas.width  = window.innerWidth-2;
-canvas.height = window.innerHeight-30;
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight-40;
 
-var tam = 10, x = canvas.width/2, y = canvas.height-30, dx = 2, dy = 2;
+var rectSize = 10, x = canvas.width/2, y = canvas.height-30, dx = 2, dy = 2, color = "#0095DD";
 
-function retangulo() {
+function changeColor(newcolor){
+    if(newcolor=="rgb") alert("Ainda tenho que fazer essa opção...");
+
+    color = newcolor;
+}
+
+function rectangle() {
     ctx.beginPath();
-    ctx.fillRect(x, y, tam, tam)
-    ctx.fillStyle = "#0095DD";
+    ctx.fillRect(x, y, rectSize, rectSize)
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
 }
 
-function desenha() {
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    retangulo();
+    rectangle();
     
-    if(x + dx > canvas.width-tam || x + dx < 0) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-tam || y + dy < 0) {
-        dy = -dy;
-    }
+    if(x + dx > canvas.width-rectSize || x + dx < 0) dx = -dx;
+    if(y + dy > canvas.height-rectSize || y + dy < 0) dy = -dy;
     
     x += dx;
     y += dy;
 }
 
 function aumenta(){
-	tam++;
+	rectSize++;
 }
 
 function diminui(){
-	tam--;
+	rectSize--;
 }
 
-setInterval(desenha, 10);
+setInterval(draw, 10);
